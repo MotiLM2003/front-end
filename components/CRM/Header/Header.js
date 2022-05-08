36f117c7eb05f51275a95084d9d1d9a5f550cca4
Moti Elmakies <motiphone2003@gmail.com>
2022-05-08 10:42:14 +0300

    :zap:progress

--- components/CRM/Header/Header.js
+++ components/CRM/Header/Header.js
@@ -1,7 +1,20 @@
+import Image from 'next/image';
 import React from 'react';
-
+import logo from '../../../images/logo-white.svg';
+import question from '../../../images/question.svg';
+import settings from '../../../images/settings.svg';
+import bell from '../../../images/bell.svg';
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='bg-primary min-h-[98px] p-4 text-`>'>
+      <div className='flex flex-row-reverse gap-3 justify-start'>
+        <Image src={logo} />
+        <Image src={question} />
+        <Image src={settings} />
+        <Image src={bell} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- pages/shared/CRMLayout.js
+++ pages/shared/CRMLayout.js
@@ -1,13 +1,36 @@
 import Footer from '@components/CRM/Footer/Footer';
 import Header from '@components/CRM/Header/Header';
+import Image from 'next/image';
 
+import status from '../../images/icons/white/status.svg';
+import heart from '../../images/icons/white/hand-heart.svg';
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div className=''>
-        <div className='grow'>{children}</div>
+      <div className='flex'>
+        <div className='basis-1/6 bg-shades-600 pl-4 py-10  rounded '>
+          <div className='flex flex-col justify-center gap-4'>
+            <div>
+              <div className='flex text-white items-center gap-3 text-xs gap-4'>
+                <Image src={status} />{' '}
+                <label htmlFor=''>Status & Balance</label>
+              </div>
+            </div>
+            <div>
+              <div className='flex text-white items-center gap-3 text-xs'>
+                <Image src={heart} /> <label htmlFor=''>Donations</label>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className=''>
+              <div className='grow'>{children}</div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <Footer />
     </>
   );
